8ac0bc40ac0abe70e0be762ddee4aff389112660
red_byte <developer.sokolov@gmail.com>
2021-04-01 23:42:30 +0300

    @feat: small change logo and show her

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-#include <iterator>
 #include <fstream>
 
-std::string get_logo() {
-    std::cout << "START" << std::endl;
-    std::string filename = "ascii_logo.txt";
-    std::string logo;
-    std::ifstream ifs{filename.c_str()};
-    std::getline(ifs, logo, '\0');
-    std::cout << logo << std::endl;
-    return logo;
+void get_logo() {
+    using namespace std;
+    string line;
+    ifstream logo_file("../resources/ascii_logo.txt"); //TODO: change to config file
+    if (logo_file.is_open()) {
+        while (getline(logo_file, line)) {
+            cout << line << '\n';
+        }
+        logo_file.close();
+    }
 }
 
 int main(int args, const char **argv) {
-
+    get_logo();
+    return 0;
 }
